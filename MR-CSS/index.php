0f3e51a96510864ca055e6db6a7d0f2a8f2319c9
Muhammad Rusdi <muhammadrusdi4@gmail.com>
2015-05-18 14:32:57 +0800

    Tesss

---
+++ MR-CSS/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>MR CSS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="stylesheets/stylesheets.css">
+	<link rel="stylesheet" href="stylesheets/globalheader.css">
+	<link rel="stylesheet" href="stylesheets/fonts.css">
+	<link rel="stylesheet" href="stylesheets/mr.built.css">
+
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</head>
+<body>
+	<nav id="globalheader" class="globalheader">
+		<div class="gh-content">
+			<div class="gh-nav">
+				<div class="gh-nav-view">
+					<ul class="gh-nav-list">
+						<li class="gh-tab"><a class="gh-tab-link" href="#"><span class="gh-tab-inner">Beranda</span></a></li>
+						<li class="gh-tab"><a class="gh-tab-link" href="#"><span class="gh-tab-inner">Dokter</span></a></li>
+						<li class="gh-tab"><a class="gh-tab-link" href="#"><span class="gh-tab-inner">Klinik</span></a></li>
+						<li class="gh-tab"><a class="gh-tab-link" href="#"><span class="gh-tab-inner">Layanan</span></a></li>
+						<li class="gh-tab"><a class="gh-tab-link" href="#"><span class="gh-tab-inner">Hubungi Kami</span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav> <!-- end globalheader -->
+
+	<div class="main-banner">
+		<div class="mb-content bg2">
+			banner
+		</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
